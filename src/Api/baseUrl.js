8b34e28d5a4584ceae44d0ed6bf7353978d41422
MohamedTarek <mt469063@gmail.com>
2023-12-25 14:56:29 +0200

    final commit

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //axios instance
-const baseUrl= axios.create({baseURL:"https://e-commerce-back-e7fz.onrender.com/"})
+const baseUrl= axios.create({baseURL:"https://e-commerce-back-e7fz.onrender.com"})
 //const baseUrl= axios.create({baseURL:"https://mohamed-tarek-e-commerce.cyclic.app"})
 //const baseUrl= axios.create({baseURL:"http://localhost:8000"})
 
